0452afaede2d4e32b6be5faee8627b3685065096
Joao Emilio <joaoemilio@devthinkers.com>
2017-10-21 22:50:22 -0200

    refactoring packages

---
+++ src/main/java/wso2apiforum/petclinic/system/WelcomeController.java
@@ -0,0 +1,14 @@
+package wso2apiforum.petclinic.system;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class WelcomeController {
+
+    @RequestMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+}
