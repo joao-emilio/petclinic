f8a7f82731b7237da6e62d470f2c2ec0f0b08aa5
Joao Emilio <joaoemilio@devthinkers.com>
2017-10-21 22:51:25 -0200

    refactoring packages

---
+++ src/main/java/me/joaoemilio/petclinic/system/WelcomeController.java
@@ -0,0 +1,14 @@
+package me.joaoemilio.petclinic.system;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class WelcomeController {
+
+    @RequestMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+}
---
+++ src/test/java/me/joaoemilio/petclinic/system/ProductionConfigurationTests.java
@@ -0,0 +1,23 @@
+package me.joaoemilio.petclinic.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import me.joaoemilio.petclinic.vet.VetRepository;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductionConfigurationTests {
+
+    @Autowired
+    private VetRepository vets;
+
+    @Test
+    public void testFindAll() throws Exception {
+        vets.findAll();
+        vets.findAll(); // served from cache
+    }
+}
